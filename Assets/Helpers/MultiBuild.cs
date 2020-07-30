5078e06261eeef4088c42092d0ff12544ac8fe0c
Sando <raphael.james14@gmail.com>
2020-07-30 17:35:46 +0200

    Adding scripts for custom builds window (To complete with content from './Assets/MRTK/Tools/BuildWindow'

---
+++ Assets/Helpers/MultiBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu]
+[System.Serializable]
+public class MultiBuild : ScriptableObject
+{
+    [SerializeField]
+    public List<SceneAsset> sceneWall = new List<SceneAsset>();
+    [SerializeField]
+    public List<SceneAsset> sceneMaster = new List<SceneAsset>();
+    [SerializeField]
+    public List<SceneAsset> sceneHL = new List<SceneAsset>();
+
+}
